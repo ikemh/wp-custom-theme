c49ca454bdc9e9140d7a66577cd76845c4a60aa6
ikemh <ikemh@hotmail.com>
2024-06-14 15:22:24 -0300

    Refactoring...

---
+++ functions/globals.php
@@ -0,0 +1,5 @@
+<?php
+$GLOBALS['storeName'***REMOVED*** = get_option('store_name', ''***REMOVED***;
+$GLOBALS['storeCnpj'***REMOVED*** = get_option('store_cnpj', ''***REMOVED***;
+$GLOBALS['storeEmail'***REMOVED*** = get_option('store_email', ''***REMOVED***;
+$GLOBALS['storePhone'***REMOVED*** = get_option('store_phone', ''***REMOVED***;
