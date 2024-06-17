bf3aa77b65c355f7f5feb2f9349a9c2749df8e38
ikemh <ikemh@hotmail.com>
2024-06-17 00:10:13 -0300

    Add modules to js structure and organize the enqueue-scripts by pages

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -2,15 +2,46 @@
 // Enqueue scripts and styles
 function dom_ofertas_theme_scripts() {
     wp_enqueue_style('dom-ofertas-style', get_stylesheet_uri());
+    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css', array(), null);
+    wp_enqueue_style('dom-ofertas-fonts', 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Ubuntu:wght@400;700&display=swap', array(), null);
+
+    // Common CSS
     wp_enqueue_style('dom-ofertas-misc', get_template_directory_uri() . '/css/misc.css', array(), null);
     wp_enqueue_style('dom-ofertas-header', get_template_directory_uri() . '/css/header.css', array(), null);
     wp_enqueue_style('dom-ofertas-footer', get_template_directory_uri() . '/css/footer.css', array(), null);
-    wp_enqueue_style('dom-ofertas-front-page', get_template_directory_uri() . '/css/front-page.css', array(), null);
-    wp_enqueue_style('dom-ofertas-single-page', get_template_directory_uri() . '/css/single-product.css', array(), null);
-    wp_enqueue_style('dom-ofertas-cart', get_template_directory_uri() . '/css/cart.css', array(), null);
-    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css', array(), null);
-    wp_enqueue_style('dom-ofertas-fonts', 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Ubuntu:wght@400;700&display=swap', array(), null);
-    wp_enqueue_script('jquery', false, array(), null, true);
-    wp_enqueue_script('dom-ofertas-script', get_template_directory_uri() . '/js/main.js', array('jquery'), null, true);
+    
+    // Common JS
+    wp_enqueue_script('common-script', get_template_directory_uri() . '/js/common.js', array('jquery'), null, true);
+
+    // Conditional CSS and JS
+    if (is_front_page()) {
+        wp_enqueue_style('dom-ofertas-front-page', get_template_directory_uri() . '/css/front-page.css', array(), null);
+        wp_enqueue_script('front-page-script', get_template_directory_uri() . '/js/front-page.js', array('jquery'), null, true);
+    }
+
+    if (is_single()) {
+        wp_enqueue_style('dom-ofertas-single-page', get_template_directory_uri() . '/css/single-product.css', array(), null);
+        wp_enqueue_script('single-product-script', get_template_directory_uri() . '/js/single-product.js', array('jquery'), null, true);
+    }
+
+    if (is_page('cart')) {
+        wp_enqueue_style('dom-ofertas-cart', get_template_directory_uri() . '/css/cart.css', array(), null);
+        // Adicione outros scripts específicos para a página do carrinho, se necessário
+    }
+
+    // Adicione outras condições conforme necessário
 }
 add_action('wp_enqueue_scripts', 'dom_ofertas_theme_scripts');
+
+// Add type="module" to scripts
+function add_module_type_to_scripts($tag, $handle, $src) {
+    // List of script handles to add type="module"
+    $module_scripts = array('common-script', 'front-page-script', 'single-product-script');
+
+    if (in_array($handle, $module_scripts)) {
+        $tag = '<script type="module" src="' . esc_url($src) . '"></script>';
+    }
+
+    return $tag;
+}
+add_filter('script_loader_tag', 'add_module_type_to_scripts', 10, 3);
--- js/modules/front-page/category-slider.js
+++ js/modules/front-page/category-slider.js
@@ -1,67 +1,65 @@
 export function initCategorySlider() {
-    document.addEventListener('DOMContentLoaded', () => {
-        const categories = document.querySelectorAll('.category-block');
+    const categories = document.querySelectorAll('.category-block');
 
-        categories.forEach(category => {
-            const products = category.querySelector('.products');
-            const product = category.querySelector('.products .product');
-            const prevButtonContainer = document.createElement('div');
-            const nextButtonContainer = document.createElement('div');
-            const prevButton = document.createElement('button');
-            const nextButton = document.createElement('button');
-            let scrollPosition = 0;
+    categories.forEach(category => {
+        const products = category.querySelector('.products');
+        const product = category.querySelector('.products .product');
+        const prevButtonContainer = document.createElement('div');
+        const nextButtonContainer = document.createElement('div');
+        const prevButton = document.createElement('button');
+        const nextButton = document.createElement('button');
+        let scrollPosition = 0;
 
-            prevButtonContainer.className = 'button-container prev-button-container';
-            nextButtonContainer.className = 'button-container next-button-container';
-            prevButton.className = 'prevButton';
-            nextButton.className = 'nextButton';
+        prevButtonContainer.className = 'button-container prev-button-container';
+        nextButtonContainer.className = 'button-container next-button-container';
+        prevButton.className = 'prevButton';
+        nextButton.className = 'nextButton';
 
-            prevButton.style.display = 'none'; // Esconde o botão "Anterior" inicialmente
+        prevButton.style.display = 'none'; // Esconde o botão "Anterior" inicialmente
 
-            const updateButtonsVisibility = () => {
-                if (scrollPosition <= 0) {
-                    prevButton.style.display = 'none';
-                } else {
-                    prevButton.style.display = 'block';
-                }
+        const updateButtonsVisibility = () => {
+            if (scrollPosition <= 0) {
+                prevButton.style.display = 'none';
+            } else {
+                prevButton.style.display = 'block';
+            }
 
-                if (scrollPosition >= (products.scrollWidth - products.clientWidth)) {
-                    nextButton.style.display = 'none';
-                } else {
-                    nextButton.style.display = 'block';
-                }
-            };
+            if (scrollPosition >= (products.scrollWidth - products.clientWidth)) {
+                nextButton.style.display = 'none';
+            } else {
+                nextButton.style.display = 'block';
+            }
+        };
 
-            prevButton.addEventListener('click', () => {
-                const productWidth = product.offsetWidth + parseInt(window.getComputedStyle(product).marginRight);
-                scrollPosition -= productWidth;
-                if (scrollPosition < 0) scrollPosition = 0;
-                products.scrollTo({ left: scrollPosition, behavior: 'smooth' });
-                updateButtonsVisibility();
-            });
-
-            nextButton.addEventListener('click', () => {
-                const productWidth = product.offsetWidth + parseInt(window.getComputedStyle(product).marginRight);
-                scrollPosition += productWidth;
-                if (scrollPosition > (products.scrollWidth - products.clientWidth)) {
-                    scrollPosition = products.scrollWidth - products.clientWidth;
-                }
-                products.scrollTo({ left: scrollPosition, behavior: 'smooth' });
-                updateButtonsVisibility();
-            });
-
-            prevButtonContainer.appendChild(prevButton);
-            nextButtonContainer.appendChild(nextButton);
-
-            const sliderButtons = document.createElement('div');
-            sliderButtons.className = 'slider-buttons';
-            sliderButtons.appendChild(prevButtonContainer);
-            sliderButtons.appendChild(nextButtonContainer);
-
-            category.style.position = 'relative'; // Certifique-se de que o pai seja relativo para posicionar os botões absolutamente
-            category.appendChild(sliderButtons);
-
-            updateButtonsVisibility(); // Chama a função para definir a visibilidade dos botões inicialmente
+        prevButton.addEventListener('click', () => {
+            const productWidth = product.offsetWidth + parseInt(window.getComputedStyle(product).marginRight);
+            scrollPosition -= productWidth;
+            if (scrollPosition < 0) scrollPosition = 0;
+            products.scrollTo({ left: scrollPosition, behavior: 'smooth' });
+            updateButtonsVisibility();
         });
+
+        nextButton.addEventListener('click', () => {
+            const productWidth = product.offsetWidth + parseInt(window.getComputedStyle(product).marginRight);
+            scrollPosition += productWidth;
+            if (scrollPosition > (products.scrollWidth - products.clientWidth)) {
+                scrollPosition = products.scrollWidth - products.clientWidth;
+            }
+            products.scrollTo({ left: scrollPosition, behavior: 'smooth' });
+            updateButtonsVisibility();
+        });
+
+        prevButtonContainer.appendChild(prevButton);
+        nextButtonContainer.appendChild(nextButton);
+
+        const sliderButtons = document.createElement('div');
+        sliderButtons.className = 'slider-buttons';
+        sliderButtons.appendChild(prevButtonContainer);
+        sliderButtons.appendChild(nextButtonContainer);
+
+        category.style.position = 'relative'; // Certifique-se de que o pai seja relativo para posicionar os botões absolutamente
+        category.appendChild(sliderButtons);
+
+        updateButtonsVisibility(); // Chama a função para definir a visibilidade dos botões inicialmente
     });
 }
